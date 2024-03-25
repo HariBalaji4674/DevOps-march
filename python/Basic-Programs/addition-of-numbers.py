d0ee88bca29e70ee4fa7c4b435130d2d28a8b80b
HariBalaji4674 <hari.balaji4674@gmail.com>
2024-03-25 11:11:49 +0530

    25-03-2024

--- python/Basic-Programs/addition-of-numbers.py
+++ python/Basic-Programs/addition-of-numbers.py
@@ -14,8 +14,21 @@ print(f"(The sum of two numbers are {n3} )")
 print(format(n3,"<3"))
 print(f"The sum of {n1} and {n2} is {n3}".format(n1,n2,n3))
 
+# Numbers
+numbers = dict()
+print(numbers)
+
+numbers = {1:'one',2:'two',3:'Three'}
+print(numbers)
+
+del numbers[3]
+print(numbers)
+
+print(1 in numbers,2 in numbers,3 in numbers)
 
 # Reverse the array
 list = [1,2,3,4,5]
 rev_list = list[::-1]  # [Start:End:Increment/Decrement/Step]
 print(rev_list)
+
+# 7008629229
