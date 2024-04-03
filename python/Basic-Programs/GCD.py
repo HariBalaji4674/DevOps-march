b8f5e1ba9af2033c3cf49cbecfe9f2ba17890f3f
HariBalaji4674 <hari.balaji4674@gmail.com>
2024-04-03 06:50:33 +0530

    03-04-2024

---
+++ python/Basic-Programs/GCD.py
@@ -0,0 +1,15 @@
+# Greatest Common Divisors or Highest Common Factors
+
+def gcd(a,b):
+  if a == 0:
+    return b
+  return gcd(b%a,a)
+
+print(gcd(36,60))
+
+def gcd(a,b):
+  if a == 0:
+    return b
+  return gcd(b%a,a)
+
+print(gcd(60,36))
